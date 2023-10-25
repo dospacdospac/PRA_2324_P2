2209e0aa637928d25b4b315ee3bc7fd7bbf8b7b8
TFAUSEM <tfausem@alumno.upv.es@ldsic-vdi14.upvnet.upv.es>
2023-10-25 18:53:42 +0200

    Chrono error

--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,6 +1,7 @@
 #include<vector>
 #include<iostream>
 #include "DyV.h"
+#include<chrono>
 
 int main(){
 	std::vector<int> v1{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -11,13 +12,25 @@ int main(){
 	float x2 = 1.6;
 	char x3 = 9;
 
+	auto start = std::chrono::system_clock::now();
 	int r1 = BusquedaBinaria(x1, v1, 0, 9);
+	auto end = std::chrono::system_clock::now();
+	std::chrono::duration<float, std::milli> duration1 = end - start;
+	
+	auto start = std::chrono::system_clock::now();
 	int r2 = BusquedaBinaria(x2, v2, 0, 9);
-	int r3 = BusquedaBinaria(x3, v3, 0, 9);
+	auto end = std::chrono::system_clock::now();
+        std::chrono::duration<float, std::milli> duration2 = end - start;
 
-	std::cout<<"En el vector 1 se ha buscado el valor "<< x1 <<", que se ha encontrado en la posición "<< r1;
-	std::cout<<"En el vector 2 se ha buscado el valor "<< x2 <<", que se ha encontrado en la posición "<< r2;
-	std::cout<<"En el vector 1 se ha buscado el valor "<< x3 <<", que se ha encontrado en la posición "<< r3;
+	auto start = std::chrono::system_clock::now();
+	int r3 = BusquedaBinaria(x3, v3, 0, 9);
+	auto end = std::chrono::system_clock::now();
+        std::chrono::duration<float, std::milli> duration3 = end - start;
+
+
+	std::cout<<"En el vector 1 se ha buscado el valor "<< x1 <<", que se ha encontrado en la posición "<< r1<<"Y ha tardado "<< duration1;
+	std::cout<<"En el vector 2 se ha buscado el valor "<< x2 <<", que se ha encontrado en la posición "<< r2<<"Y ha tardado "<< duration2;
+	std::cout<<"En el vector 1 se ha buscado el valor "<< x3 <<", que se ha encontrado en la posición "<< r3<<"Y ha tardado "<< duration3;
 
 	return 0;
 }
