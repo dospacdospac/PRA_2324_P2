b2bcb4dcbaa3eaf7402d52ea2c718b731da2e116
TFAUSEM <tfausem@alumno.upv.es@ldsic-vdi14.upvnet.upv.es>
2023-10-25 18:42:23 +0200

    template y test hechos

--- DyV.h
+++ DyV.h
@@ -1,9 +1,9 @@
 #include<vector>
-#include<stdio.h>
 #include <iostream>
 
 
-int BusquedaBinaria(int X, std::vector<int>& V, int ini, int fin){
+template <typename T>
+T BusquedaBinaria(int X, std::vector<T>& V, int ini, int fin){
 	if(ini > fin)
 		std::cout<<"No se encuentra en el array";
 	int medio = (ini + fin) / 2;
@@ -17,4 +17,33 @@ int BusquedaBinaria(int X, std::vector<int>& V, int ini, int fin){
 	else{
 		return BusquedaBinaria(X, V, ini, medio + 1);
 	}
+}
+
+template <typename T>
+T QuickSort(std::vector<T>& V, int ini, int fin){
+	if(ini < fin){
+		int pivot = Partition(V, ini, fin);
+		QuickSort(V, ini, pivot -1);
+		QuickSort(V, pivot + 1, fin);
+}
+}
+
+template <typename T>
+T Partition(std::vector<T>& V, int ini, int fin){
+	T x = V[fin];
+	T i = ini;
+	for(T j = ini; j == fin -1; j++){
+		if(V[j] <= x){
+			int aux;
+			aux = V[i];
+			V[i] = V[j];
+			V[j] = aux;
+			i++;
+		}
+	}
+	T aux2;
+	aux2 = V[i];
+        V[i] = V[fin];
+        V[fin] = aux2;
+	return i;
 }
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -2,7 +2,7 @@
 #include<iostream>
 #include "DyV.h"
 
-void main(){
+int main(){
 	std::vector<int> v1{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 	std::vector<float> v2{1, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9};
 	std::vector<char> v3{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -18,4 +18,7 @@ void main(){
 	std::cout<<"En el vector 1 se ha buscado el valor "<< x1 <<", que se ha encontrado en la posición "<< r1;
 	std::cout<<"En el vector 2 se ha buscado el valor "<< x2 <<", que se ha encontrado en la posición "<< r2;
 	std::cout<<"En el vector 1 se ha buscado el valor "<< x3 <<", que se ha encontrado en la posición "<< r3;
+
+	return 0;
 }
+
