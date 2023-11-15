253c1012a5601afd3ac3bd90af1c0c5a0a69db6d
TFAUSEM <tfausem@alumno.upv.es@ldsic-vdi14.upvnet.upv.es>
2023-11-15 18:52:48 +0100

    Monedas acabar

---
+++ monedas2.cpp
@@ -0,0 +1,54 @@
+
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+
+
+int cambio(std::vector<int>& v, int size, int M){
+	const int None = -1;
+	int T [size][M + 1];
+	
+	for (int i = 0; i < size; i++) {
+	       	for (int j = 0; j <= M+1; j++) {
+			T[i][j] = None;
+	       	}
+	}
+	
+	for (int j = 1; j <= M+1; j++) {
+        	if (j >= v[0] && T[0][j - v[0]] != 0) {
+            		T[0][j] = 1 + T[0][j - v[0]];
+       		 }
+   	 }
+
+	for (int i = 1; i <= size;  i++){
+		T[i][0] = 0;
+		for(int j = 1; j <= M+1; j++) {
+			if (j < v[i] || T[i][j - v[i]] == None)
+				T[i][j] = T[i -1][j];
+			else if (T[i - 1][j] != None)
+				T[i][j] = std::min(T[i -1][j], 1 + T[i][j - v[i]]);
+			else
+				T[i][j] = 1 + T[i][j - v[i]];
+		}
+
+	}
+
+	return T[v.size() - 1][M];
+
+}
+
+
+int main(){
+	
+	std::vector<int> v = {1, 4, 6};
+	int M = 8;
+
+	std::cout<< cambio(v, v.size(), M) << std::endl;
+
+	return cambio(v, v.size(), M);
+
+}
+
